c18e08369d8630b9f1ea3a824d55d6bfa00c63d8
MIracleyin <miracleyin@live.com>
2022-11-12 23:27:07 +0800

    init simple httpie

---
+++ src/main.rs
@@ -0,0 +1,173 @@
+use std::{collections::HashMap, str::FromStr, usize::MIN};
+
+use anyhow::{anyhow, Ok, Result};
+use clap::{Args, Parser, Subcommand};
+use colored::Colorize;
+use mime::Mime;
+use reqwest::{header, Client, Error, Response, Url};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(name = "Httpie")]
+#[command(author = "Yin Zhang. <miracleyin@live.com>")]
+#[command(version = "1.0")]
+#[command(about = "Http tool", long_about = None)]
+#[command(propagate_version = true)]
+struct Opts {
+    #[command(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    Get(Get),
+    Post(Post),
+}
+
+// get
+#[derive(Args, Debug)]
+struct Get {
+    #[arg(value_parser = parse_url)]
+    url: String,
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
+}
+
+// post
+#[derive(Args, Debug)]
+struct Post {
+    #[arg(value_parser = parse_url)]
+    url: String,
+    #[arg(value_parser = parse_kv_pair)]
+    body: Vec<KvPair>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split('=');
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    print_resp(resp).await?;
+    Ok(())
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+
+    let resp = client.post(&args.url).json(&body).send().await?;
+    print_resp(resp).await?;
+    Ok(())
+}
+
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}\n", name.to_string().green(), value);
+    }
+
+    print!("\n");
+}
+
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+        _ => println!("{}", body),
+    }
+}
+
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mine = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mine, &body);
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts = Opts::parse();
+    let mut headers = header::HeaderMap::new();
+
+    headers.insert("X-POWERED-BY", "RUST".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    let client = Client::builder().default_headers(headers).build()?;
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
+    };
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abs").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://httpbin.org/post").is_ok());
+    }
+
+    #[test]
+    fn parse_kv_pair_wroks() {
+        assert!(parse_kv_pair("a").is_err());
+        assert_eq!(
+            parse_kv_pair("a=1").unwrap(),
+            KvPair {
+                k: "a".into(),
+                v: "1".into(),
+            }
+        );
+
+        assert_eq!(
+            parse_kv_pair("b=").unwrap(),
+            KvPair {
+                k: "b".into(),
+                v: "".into(),
+            }
+        )
+
+    }
+}
